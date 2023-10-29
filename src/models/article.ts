a8e004a106999180bce5770a83bee13cd88e70c9
pouriya-moien <111745220+pouriya-moien@users.noreply.github.com>
2023-10-29 13:06:08 -0700

    intarfeces

---
+++ src/models/article.ts
@@ -0,0 +1,8 @@
+export interface article {
+    id: number,
+    title: string,
+    date: Date,
+    infos: string,
+    dec: string,
+    img: string
+}
---
+++ src/models/doctors.ts
@@ -0,0 +1,7 @@
+export interface doctors {
+    id: number,
+    name: string,
+    img: string,
+    social: [],
+    exppert: string
+}
